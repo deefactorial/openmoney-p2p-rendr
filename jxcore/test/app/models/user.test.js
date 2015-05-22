51764d1e532f3412298dc2bb54965398810cb32c
deefactorial <deefactorial@gmail.com>
2015-05-22 13:43:39 -0700

    update tests

--- jxcore/test/app/models/user.test.js
+++ jxcore/test/app/models/user.test.js
@@ -1,11 +1,15 @@
+
 var should = require("should");
 var User = require('../../../app/models/user');
 
-describe('User', function() {
 
-  it('should use login as the model id', function() {
-    var user = new User({login: 'someusername'});
-    user.id.should.equal('someusername');
+if(typeof describe !== 'undefined') {
+  describe('User', function () {
+
+    it('should use login as the model id', function () {
+      var user = new User({login: 'someusername'});
+      user.id.should.equal('someusername');
+    });
+
   });
-
-});
+}
